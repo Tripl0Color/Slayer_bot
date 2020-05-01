b3ba6237da79edfdb43f830b7980d0e7ff8108a9
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-05-01 03:16:19 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,128 @@
+from random import randint
+import requests
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import pyowm
+import datetime
+from pycbrf.toolbox import ExchangeRates
+from nickname_generator import generate
+
+
+
+vk_session = vk_api.VkApi(token='token')
+
+owm = pyowm.OWM('token',language = "RU")
+
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+ban_list = ['хуй','Хуй','пизда','Сука','еблан','дебил','идиот','пезда','хуесос','пиздется','хуево','cock','idiot','eblan','хуело']
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        if event.text in ban_list:
+            if event.from_user:
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Не матерись'
+		)
+            elif event.from_chat:
+                vk.messages.send(
+                    chat_id=event.chat_id,
+                    random_id=event.random_id,
+                    message='Не матерись'
+        )
+
+        if event.text == 'погода' or event.text == 'Погода':
+            if event.from_user:
+                vk.messages.send(
+                    user_id=event.user_id,
+                    random_id=event.random_id,
+                    message='Введите город🌏'
+		)
+            elif event.from_chat:
+                vk.messages.send(
+                    chat_id=event.chat_id,
+                    random_id=event.random_id,
+                    message='Введите город🌏'
+       )
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+                    observation = owm.weather_at_place(event.text)
+                    w = observation.get_weather()
+                    temp = w.get_temperature('celsius')["temp"]
+                    if event.from_user:
+                        vk.messages.send(
+                            user_id=event.user_id,
+                            random_id=event.random_id,
+                            message="В городе " +  event.text  + "🌆  " + "сейчас " + w.get_detailed_status() +'.' + "\nTемпература " + str(temp)+" °C"
+        		)
+                    break
+                elif event.from_chat:
+                    observation = owm.weather_at_place(event.text)
+                    w = observation.get_weather()
+                    temp = w.get_temperature('celsius')["temp"]
+                    vk.messages.send(
+                        chat_id=event.chat_id,
+                        random_id=event.random_id,
+                        message="В городе " +  event.text  + "🌆  " + "сейчас " + w.get_detailed_status() +'.' + "\nTемпература " + str(temp)+" °C"
+        	    )
+                    break
+
+        if event.text == 'Курс' or event.text == 'курс':
+            #получение курса
+            time = datetime.datetime.now()
+            rates = ExchangeRates(time)
+
+            cource_uah = rates['UAH'].rate
+
+            cource_czk = rates['CZK'].rate
+
+            cource_jpy = rates['JPY'].rate
+
+            course = rates['USD'].rate
+
+            course_eur = rates['EUR'].rate
+            if event.from_user:
+                vk.messages.send(
+                user_id=event.user_id,
+                random_id=event.random_id,
+                message='Курс\nДоллар ' + str(course) + '\n' + 'Евро ' + str(course_eur) + '\n' + 'Гривна ' +  str(cource_uah) + '\n' + 'Чеш.крона ' + str(cource_czk) + '\n' + 'Япон.иена ' + str(cource_jpy)
+        		)
+
+
+
+        if event.text == '/help' or event.text == 'помощь':
+            vk.messages.send(
+            user_id=event.user_id,
+            random_id=event.random_id,
+            message='В этой статье описаны все комманды бота - https://vk.com/@-194867465-komandy-bota'
+            )
+
+        if event.text == 'Ник' or event.text == 'ник':
+           vk.messages.send(
+           user_id=event.user_id,
+           random_id=event.random_id,
+           message='Сгенерировал ' + generate()
+           )
+
+        if event.text == 'Размер' or event.text == 'размер':
+          cock = randint(1,20)
+          vk.messages.send(
+          user_id=event.user_id,
+          random_id=event.random_id,
+          message='Твой  член ' + str(cock) + 'cm'
+         )
+
+        if event.text == 'slayer' or event.text == 'Слейр':
+            vk.messages.send(
+            user_id=event.user_id,
+            random_id=event.random_id,
+            message='я тут'
+          )
+
+        else:
+           vk.messages.send(
+           user_id=event.user_id,
+           random_id=event.random_id,
+           message='Каво?Список комманд - https://vk.com/@-194867465-komandy-bota'
+        )
